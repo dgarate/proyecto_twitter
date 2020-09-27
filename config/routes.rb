f02d3534224c77b56ea3ad0a43dea604ee683a20
dgarate <garatex@gmail.com>
2020-09-26 23:20:41 -0300

    profile page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   
   resources :tweets
   
-   
+  resources :users, only: [:profile] do
+    get 'profile'
+  end 
+
   #get 'home/index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
